3e7a9268864c7060e9f0b91bf12df0c8abd84565
2nthony <ant.hu@outlook.com>
2024-09-18 02:40:50 +0800

    use element size

---
+++ packages/core/useElementSize/fixture.tsx
@@ -0,0 +1,27 @@
+import { useSignal } from '@signals-use/shared'
+import type { LegacyRef } from 'react'
+import { useElementSize } from '.'
+
+export default function DemoUseElementSize() {
+  const ref = useSignal<HTMLTextAreaElement | null>(null)
+  const text = useSignal('')
+
+  const { width, height } = useElementSize(
+    ref,
+    { width: 0, height: 0 },
+    { box: 'border-box' },
+  )
+
+  text.value = `width: ${width}\nheight: ${height}`
+
+  return (
+    <div>
+      <p>Resize the box to see changes</p>
+
+      <textarea
+        ref={ref as unknown as LegacyRef<HTMLTextAreaElement>}
+        value={text.value}
+      />
+    </div>
+  )
+}
