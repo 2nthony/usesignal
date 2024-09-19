f061a8643ba7f630bead29b4461acbf8ce23db5e
2nthony <ant.hu@outlook.com>
2024-09-19 21:51:59 +0800

    use async state

---
+++ packages/core/src/use-async-state/fixture.tsx
@@ -0,0 +1,44 @@
+import { useSignal } from '@usesignal/shared'
+import { useAsyncState } from '.'
+
+export default function DemoUseAsyncState() {
+  const id = useSignal(1)
+
+  const { state, isReady, isLoading, execute } = useAsyncState(
+    (params: { id: number }) => {
+      const id = params?.id ?? 1
+
+      return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(resp => resp.json())
+    },
+    { id: null },
+    {
+      delay: 2000,
+      resetOnExecute: false,
+    },
+  )
+
+  return (
+    <div>
+      <div>
+        Ready:
+        {isReady.toString()}
+      </div>
+      <div>
+        Loading:
+        {isLoading.toString()}
+      </div>
+      <div>
+        <pre>{JSON.stringify(state.value, null, 2)}</pre>
+      </div>
+      <button
+        onClick={() => {
+          id.value += 1
+          execute(2000, { id: id.value })
+        }}
+      >
+        Execute
+      </button>
+    </div>
+  )
+}
---
+++ packages/core/src/use-async-state/index.ts
@@ -0,0 +1,130 @@
+import type { Signal } from '@preact/signals-react'
+import { noop, promiseTimeout, useSignal } from '@usesignal/shared'
+import { useOnMounted } from '../use-on-mounted'
+
+export interface UseAsyncStateReturn<Data, Params extends any[]> {
+  state: Signal<Data>
+  isReady: Signal<boolean>
+  isLoading: Signal<boolean>
+  error: Signal<unknown>
+  execute: (delay?: number, ...args: Params) => Promise<Data>
+}
+
+export interface UseAsyncStateOptions<D = any> {
+  /**
+   * Delay for executing the promise. In milliseconds.
+   *
+   * @default 0
+   */
+  delay?: number
+
+  /**
+   * Execute the promise right after the function is invoked.
+   * Will apply the delay if any.
+   *
+   * When set to false, you will need to execute it manually.
+   *
+   * @default true
+   */
+  immediate?: boolean
+
+  /**
+   * Callback when error is caught.
+   */
+  onError?: (e: unknown) => void
+
+  /**
+   * Callback when success is caught.
+   * @param {D} data
+   */
+  onSuccess?: (data: D) => void
+
+  /**
+   * Sets the state to initialState before executing the promise.
+   *
+   * This can be useful when calling the execute function more than once (for
+   * example, to refresh data). When set to false, the current state remains
+   * unchanged until the promise resolves.
+   *
+   * @default true
+   */
+  resetOnExecute?: boolean
+
+  /**
+   *
+   * An error is thrown when executing the execute function
+   *
+   * @default false
+   */
+  throwError?: boolean
+}
+
+export function useAsyncState<Data, Params extends any[] = []>(
+  promise: (...args: Params) => Promise<Data>,
+  initialState: Data,
+  options?: UseAsyncStateOptions<Data>,
+): UseAsyncStateReturn<Data, Params> {
+  const {
+    delay = 0,
+    immediate = true,
+    onError = noop,
+    onSuccess = noop,
+    resetOnExecute = true,
+    throwError = false,
+  } = options ?? {}
+
+  const state = useSignal(initialState)
+  const isReady = useSignal(false)
+  const isLoading = useSignal(false)
+  const error = useSignal<unknown>(null)
+
+  async function execute(delay = 0, ...args: any[]) {
+    if (resetOnExecute) {
+      state.value = initialState
+    }
+    error.value = null
+    isReady.value = false
+    isLoading.value = true
+
+    if (delay > 0) {
+      await promiseTimeout(delay)
+    }
+
+    const _promise = promise(...args as Params)
+
+    try {
+      const data = await _promise
+      state.value = data
+      isReady.value = true
+      onSuccess(data)
+    }
+    catch (e) {
+      error.value = e
+      onError(e)
+      if (throwError) {
+        throw e
+      }
+    }
+    finally {
+      isLoading.value = false
+    }
+
+    return state.value as Data
+  }
+
+  useOnMounted(() => {
+    if (immediate) {
+      execute(delay)
+    }
+  })
+
+  const result = {
+    state,
+    isReady,
+    isLoading,
+    error,
+    execute,
+  }
+
+  return result
+}
