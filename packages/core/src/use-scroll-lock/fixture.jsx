aae79787f0c2a57148452bba793c00cf7ba41f42
2nthony <ant.hu@outlook.com>
2024-09-21 20:52:35 +0800

    use scroll lock

---
+++ packages/core/src/use-scroll-lock/fixture.jsx
@@ -0,0 +1,31 @@
+import { useScrollLock } from '.'
+import { useToggle } from '../use-toggle'
+import { useSignal } from '../utils'
+
+export default function DemoUseScrollLock() {
+  const ref = useSignal()
+
+  const isLocked = useScrollLock(ref)
+  const toggleLock = useToggle(isLocked)
+
+  return (
+    <div>
+      <div
+        ref={ref}
+        style={{ width: 300, height: 300, overflow: 'scroll', border: '1px solid gray' }}
+      >
+        <div style={{ width: 600, height: 600, margin: '200px 0 0 200px' }}>
+          scroll me
+        </div>
+      </div>
+
+      <button
+        onClick={() => {
+          toggleLock()
+        }}
+      >
+        toggle lock
+      </button>
+    </div>
+  )
+}
