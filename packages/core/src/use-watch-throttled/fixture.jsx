a843bfe49d3312f5f17f62fb7dcf5f3bf55f57a8
2nthony <ant.hu@outlook.com>
2024-09-21 18:05:08 +0800

    use watch throttled

---
+++ packages/core/src/use-watch-throttled/fixture.jsx
@@ -0,0 +1,35 @@
+import { useWatchThrottled } from '.'
+import { useSignal } from '../utils'
+
+export default function DemoUseWatchThrottled() {
+  const input = useSignal('')
+  const text = useSignal(input.value)
+  const count = useSignal(0)
+
+  useWatchThrottled(
+    input,
+    () => {
+      text.value = input.value
+      count.value += 1
+    },
+    {
+      throttle: 1000,
+    },
+  )
+  return (
+    <div>
+      <div>
+        <input type="text" value={input} onChange={e => input.value = e.target.value} />
+        <p>Delay is set to 1000ms for this demo.</p>
+        <p>
+          input:
+          {text}
+        </p>
+        <p>
+          Times updated:
+          {count}
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ packages/core/src/use-watch-throttled/index.ts
@@ -0,0 +1,32 @@
+import type { WatchCallback, WatchOptions, WatchSource } from '../use-watch'
+import type { MaybeSignalOrGetter } from '../utils'
+import { useWatchWithFilter } from '../use-watch-with-filter'
+import { throttleFilter } from '../utils/filter'
+
+export interface WatchThrottledOptions extends WatchOptions {
+  throttle?: MaybeSignalOrGetter<number>
+  trailing?: boolean
+  leading?: boolean
+}
+
+export function useWatchThrottled<T>(
+  source: WatchSource<T>,
+  cb: WatchCallback,
+  options: WatchThrottledOptions = {},
+) {
+  const {
+    throttle = 0,
+    trailing = true,
+    leading = true,
+    ...watchOptions
+  } = options
+
+  return useWatchWithFilter(
+    source,
+    cb,
+    {
+      ...watchOptions,
+      eventFilter: throttleFilter(throttle, trailing, leading),
+    },
+  )
+}
