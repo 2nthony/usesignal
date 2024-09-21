7aed01a989772c5bc366ffb8f29db9edc1b4bfc6
2nthony <ant.hu@outlook.com>
2024-09-21 15:04:39 +0800

    to signal

---
+++ packages/core/src/utils/to-signal/index.ts
@@ -0,0 +1,7 @@
+import type { MaybeSignalOrGetter } from '../types'
+import { computed } from '@preact/signals-react'
+import { signal } from '../signals'
+
+export function toSignal<T>(r: MaybeSignalOrGetter<T>) {
+  return (typeof r === 'function' ? computed : signal)(r as any)
+}
