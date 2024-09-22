b08650b0f6cfaa007053facfdda1389b04cb7696
2nthony <ant.hu@outlook.com>
2024-09-23 03:21:20 +0800

    use time ago

---
+++ packages/core/src/use-time-ago/fixture.jsx
@@ -0,0 +1,30 @@
+'use client'
+import { useComputed } from '@preact/signals-react'
+import { useTimeAgo } from '.'
+import { useSignal } from '../utils'
+
+export default function DemoUseTimeAgo() {
+  const slider = useSignal(0)
+  const sliderMS = useComputed(() => slider.value ** 3)
+  const time = useComputed(() => Date.now() + sliderMS.value)
+  const timeAgo = useTimeAgo(time)
+
+  return (
+    <div>
+      <div>
+        { timeAgo }
+      </div>
+      <input
+        value={slider}
+        onChange={e => slider.value = e.target.value}
+        type="range"
+        min="-3800"
+        max="3800"
+      />
+      <div>
+        { sliderMS }
+        ms
+      </div>
+    </div>
+  )
+}
