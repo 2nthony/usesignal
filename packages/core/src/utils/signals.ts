59bb366b3e9725eba6e9b2b0ba19fd1e19108227
2nthony <ant.hu@outlook.com>
2024-09-24 00:44:36 +0800

    update signals, mainly support computed

--- packages/core/src/utils/signals.ts
+++ packages/core/src/utils/signals.ts
@@ -1,7 +1,7 @@
 'use client'
-import type { Signal } from '@preact/signals-react'
-import type { MaybeSignal } from '../utils'
-import { signal as _signal } from '@preact/signals-react'
+import type { ReadonlySignal, Signal } from '@preact/signals-react'
+import type { AnyFn, MaybeSignal, MaybeSignalOrGetter } from '../utils'
+import { computed as _computed, signal as _signal } from '@preact/signals-react'
 import { useMemo } from 'react'
 
 export function isSignal(s: any): s is Signal {
@@ -11,23 +11,32 @@ export function isSignal(s: any): s is Signal {
 /**
  * Create a signal, compat `useRef.current` and `signal` itself
  */
-export function signal<T>(v?: MaybeSignal<T>) {
-  const valueIsSignal = isSignal(v)
-  const value = valueIsSignal ? v : _signal(v)
-
-  // useRef compat
-  if (!('current' in value)) {
-    Object.defineProperty(value, 'current', {
-      get() {
-        return value.value
-      },
-      set(newValue) {
-        value.value = newValue
-      },
-    })
+export function signal<T>(v?: MaybeSignalOrGetter<T>) {
+  if (typeof v === 'function') {
+    v = (v as AnyFn)()
   }
 
-  return value as Signal<T>
+  const valueIsSignal = isSignal(v)
+  const value = (valueIsSignal ? v : _signal(v)) as Signal<T>
+
+  ;(value as any).current = (value as any).current || null
+
+  return new Proxy(value, {
+    get(target, key) {
+      if (key === 'current') {
+        return Reflect.get(target, 'value')
+      }
+
+      return Reflect.get(target, key)
+    },
+    set(target, key, value) {
+      if (key === 'current') {
+        return Reflect.set(target, 'value', value)
+      }
+
+      return Reflect.set(target, key, value)
+    },
+  })
 }
 
 /**
@@ -35,4 +44,41 @@ export function signal<T>(v?: MaybeSignal<T>) {
  */
 export function useSignal<T>(v?: MaybeSignal<T>) {
   return useMemo(() => signal(v), [])
+}
+
+interface ComputedOptions<T> {
+  get: () => T
+  set: (value: any) => void
+}
+export interface ComputedSignal<T> extends ReadonlySignal<T> {}
+export function computed<T>(getter: () => T): ComputedSignal<T>
+export function computed<T>(options: ComputedOptions<T>): ComputedSignal<T>
+export function computed<T>(val: (() => T) | ComputedOptions<T>) {
+  let setter: ComputedOptions<T>['set'] | undefined
+  let getter
+
+  if (typeof val === 'function') {
+    getter = val
+  }
+  else {
+    getter = val.get
+    setter = val.set
+  }
+
+  const signal = _computed(getter) as ComputedSignal<T>
+
+  return new Proxy(signal, {
+    get(target, key) {
+      return Reflect.get(target, key)
+    },
+    set(_target, _key, value) {
+      if (setter) {
+        setter(value)
+
+        return true
+      }
+
+      return false
+    },
+  })
 }
