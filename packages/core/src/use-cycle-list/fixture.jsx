c17e136d74421999b6eeb57ea4b16566165fb59f
2nthony <ant.hu@outlook.com>
2024-09-24 00:45:36 +0800

    use cycle list

---
+++ packages/core/src/use-cycle-list/fixture.jsx
@@ -0,0 +1,40 @@
+'use client'
+import { useCycleList } from '.'
+import { useSignal } from '../utils'
+
+export default function DemoUseCycleList() {
+  const list = useSignal([
+    'Dog',
+    'Cat',
+    'Lizard',
+    'Shark',
+    'Whale',
+    'Dolphin',
+    'Octopus',
+    'Seal',
+  ])
+
+  const { state, next, prev } = useCycleList(list)
+
+  return (
+    <div>
+      <div>
+        { state }
+      </div>
+      <button
+        onClick={() => {
+          prev()
+        }}
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => {
+          next()
+        }}
+      >
+        Next
+      </button>
+    </div>
+  )
+}
---
+++ packages/core/src/use-cycle-list/index.ts
@@ -0,0 +1,100 @@
+'use client'
+import type { Signal } from '@preact/signals-react'
+import type { MaybeSignal, MaybeSignalOrGetter } from '../utils'
+import { useComputed } from '../use-computed'
+import { useWatch } from '../use-watch'
+import { toValue, useSignal } from '../utils'
+
+export interface UseCycleListReturn<T> {
+  state: Signal<T>
+  index: Signal<number>
+  next: (n?: number) => T
+  prev: (n?: number) => T
+  /**
+   * Go to a specific index
+   */
+  go: (i: number) => T
+}
+
+export interface UseCycleListOptions<T> {
+  /**
+   * The initial value of the state.
+   * A ref can be provided to reuse.
+   */
+  initialValue?: MaybeSignal<T>
+
+  /**
+   * The default index when
+   */
+  fallbackIndex?: number
+
+  /**
+   * Custom function to get the index of the current value.
+   */
+  getIndexOf?: (value: T, list: T[]) => number
+}
+
+/**
+ * Cycle through a list of items
+ *
+ * @see https://vueuse.org/useCycleList
+ */
+export function useCycleList<T>(list: MaybeSignalOrGetter<T[]>, options?: UseCycleListOptions<T>): UseCycleListReturn<T> {
+  const state = useSignal(getInitialValue()) as Signal<T>
+  const listRef = useSignal(list) as Signal<T[]>
+
+  const index = useComputed<number>({
+    get() {
+      const targetList = toValue(listRef)
+
+      let index = options?.getIndexOf
+        ? options.getIndexOf(state.value, targetList)
+        : targetList.indexOf(state.value)
+
+      if (index < 0) {
+        index = options?.fallbackIndex ?? 0
+      }
+
+      return index
+    },
+    set(v) {
+      set(v)
+    },
+  })
+
+  function set(i: number) {
+    const targetList = toValue(listRef)
+    const length = targetList.length
+    const index = (i % length + length) % length
+
+    const value = targetList[index]
+    state.value = value
+    return value
+  }
+
+  function shift(delta = 1) {
+    return set(index.value + delta)
+  }
+
+  function next(n = 1) {
+    return shift(n)
+  }
+
+  function prev(n = 1) {
+    return shift(-n)
+  }
+
+  function getInitialValue() {
+    return toValue(options?.initialValue ?? toValue<T[]>(list)[0]) ?? undefined
+  }
+
+  useWatch(listRef, () => set(index.value))
+
+  return {
+    state,
+    index,
+    next,
+    prev,
+    go: set,
+  }
+}
