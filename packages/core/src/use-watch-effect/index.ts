dccd2d3dfe3d3ab35f0adab8d840b4b538562061
2nthony <ant.hu@outlook.com>
2024-09-24 00:38:25 +0800

    use watch effect

---
+++ packages/core/src/use-watch-effect/index.ts
@@ -0,0 +1,9 @@
+'use client'
+import { useSignalEffect } from '@preact/signals-react'
+
+/**
+ * Rename export useSignalEffect to useWatchEffect
+ */
+export function useWatchEffect(...args: Parameters<typeof useSignalEffect>) {
+  return useSignalEffect(...args)
+}
