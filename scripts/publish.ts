cea5dd16b36e3b0f4ea658e798bc6ba08addcf62
2nthony <ant.hu@outlook.com>
2024-09-25 01:00:08 +0800

    ci: fix workflow

---
+++ scripts/publish.ts
@@ -0,0 +1,18 @@
+import { execSync } from 'node:child_process'
+import path from 'node:path'
+import { version } from '../package.json'
+
+// TODO: make this dynamic
+const packages = ['core']
+
+execSync('npm run build', { stdio: 'inherit' })
+
+let command = 'npm publish --access public'
+
+if (version.includes('beta'))
+  command += ' --tag beta'
+
+for (const name of packages) {
+  execSync(command, { stdio: 'inherit', cwd: path.join('packages', name, 'dist') })
+  console.info(`Published @vueuse/${name}`)
+}
