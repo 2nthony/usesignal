d8a4dd88473a6a0e1d119d0ab382e0f1d6d14a49
2nthony <ant.hu@outlook.com>
2024-09-25 00:38:32 +0800

    feat: enhance element utilities with computed signal support

--- packages/core/src/utils/element.ts
+++ packages/core/src/utils/element.ts
@@ -1,5 +1,18 @@
-import type { MaybeReadonlySignal, MaybeSignal } from './types'
+import type { MaybeComputedSignal, MaybeReadonlySignal, MaybeSignal } from './types'
+import { toValue } from './to-value'
 
 export type MaybeElementSignal<T extends MaybeElement = MaybeElement> = MaybeSignal<T>
 export type MaybeElementReadonlySignal<T extends MaybeElement = MaybeElement> = MaybeReadonlySignal<T>
 export type MaybeElement = HTMLElement | SVGElement | undefined | null
+export type MaybeComputedElementSignal<T extends MaybeElement = MaybeElement> = MaybeComputedSignal<T>
+
+export type UnSignalElementReturn<T extends MaybeElement = MaybeElement> = T
+
+/**
+ * Get the dom element of a signal of element
+ *
+ * @param el
+ */
+export function toElement<T extends MaybeElement>(el: MaybeComputedElementSignal<T>): UnSignalElementReturn<T> {
+  return toValue(el)
+}
