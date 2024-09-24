b2377095d187186dba8e5f82118d7fef157384cd
2nthony <ant.hu@outlook.com>
2024-09-25 00:39:42 +0800

    feat: add useIntersectionObserver

---
+++ packages/core/src/use-intersection-observer/fixture.jsx
@@ -0,0 +1,74 @@
+'use client'
+import { useIntersectionObserver } from '.'
+import { useSignal } from '../signals'
+
+export default function DemoUseIntersectionObserver() {
+  const root = useSignal(null)
+  const target = useSignal(null)
+  const isVisible = useSignal(false)
+
+  const { isActive, pause, resume } = useIntersectionObserver(
+    target,
+    ([{ isIntersecting }]) => {
+      isVisible.value = isIntersecting
+    },
+    { root },
+  )
+
+  return (
+    <div>
+      <div>
+        <label>
+          <input
+            checked={isActive.value}
+            type="checkbox"
+            name="enabled"
+            onChange={(event) => {
+              if (event.target.checked) {
+                resume()
+              }
+              else {
+                pause()
+              }
+            }}
+          />
+          <span>Enable</span>
+        </label>
+      </div>
+
+      <div
+        ref={root}
+        style={{
+          height: 200,
+          overflowY: 'scroll',
+          border: '2px dashed #ccc',
+        }}
+      >
+        <p
+          style={{
+            textAlign: 'center',
+            marginBottom: 300,
+          }}
+        >
+          Scroll me down!
+        </p>
+        <div
+          ref={target}
+          style={{
+            maxHeight: 150,
+            margin: '0 2rem 400px',
+            border: '2px solid #ccc',
+            textAlign: 'center',
+          }}
+        >
+          <p>Hello world!</p>
+        </div>
+      </div>
+      <div>
+        {isVisible.value ? 'inside' : 'outside'}
+        { ' ' }
+        the viewport
+      </div>
+    </div>
+  )
+}
---
+++ packages/core/src/use-intersection-observer/index.ts
@@ -0,0 +1,124 @@
+import type { Signal } from '@preact/signals-react'
+import type { ConfigurableWindow } from '../_configurable'
+import type { MaybeComputedElementSignal, MaybeElement, MaybeSignalOrGetter, Pausable } from '../utils'
+import { defaultWindow } from '../_configurable'
+import { useComputed, useSignal } from '../signals'
+import { useOnCleanup } from '../use-on-cleanup'
+import { useSupported } from '../use-supported'
+import { useWatch } from '../use-watch'
+import { noop, notNullish, toElement, toValue } from '../utils'
+
+export interface UseIntersectionObserverOptions extends ConfigurableWindow {
+  /**
+   * Start the IntersectionObserver immediately on creation
+   *
+   * @default true
+   */
+  immediate?: boolean
+
+  /**
+   * The Element or Document whose bounds are used as the bounding box when testing for intersection.
+   */
+  root?: MaybeComputedElementSignal | Document
+
+  /**
+   * A string which specifies a set of offsets to add to the root's bounding_box when calculating intersections.
+   */
+  rootMargin?: string
+
+  /**
+   * Either a single number or an array of numbers between 0.0 and 1.
+   * @default 0
+   */
+  threshold?: number | number[]
+}
+
+export interface UseIntersectionObserverReturn extends Pausable {
+  isSupported: Signal<boolean>
+  stop: () => void
+}
+
+/**
+ * Detects that a target element's visibility.
+ *
+ * @see https://vueuse.org/useIntersectionObserver
+ * @param target
+ * @param callback
+ * @param options
+ */
+export function useIntersectionObserver(
+  target: MaybeComputedElementSignal | MaybeSignalOrGetter<MaybeElement[]> | MaybeComputedElementSignal[],
+  callback: IntersectionObserverCallback,
+  options: UseIntersectionObserverOptions = {},
+): UseIntersectionObserverReturn {
+  const {
+    root,
+    rootMargin = '0px',
+    threshold = 0,
+    window = defaultWindow,
+    immediate = true,
+  } = options
+
+  const isSupported = useSupported(() => window && 'IntersectionObserver' in window)
+  const targets = useComputed(() => {
+    const _target = toValue(target)
+    return (Array.isArray(_target) ? _target : [_target]).map(toElement).filter(notNullish)
+  })
+
+  let cleanup = noop
+  const isActive = useSignal(immediate)
+
+  const stopWatch = isSupported.value
+    ? useWatch(
+      () => [targets.value, toElement(root as MaybeComputedElementSignal), isActive.value] as const,
+      ([targets, root]: [MaybeElement[], MaybeElement]) => {
+        cleanup()
+        if (!isActive.value) {
+          return
+        }
+
+        if (!targets.length) {
+          return
+        }
+
+        const observer = new IntersectionObserver(
+          callback,
+          {
+            root: toElement(root),
+            rootMargin,
+            threshold,
+          },
+        )
+
+        targets.forEach(el => el && observer.observe(el))
+
+        cleanup = () => {
+          observer.disconnect()
+          cleanup = noop
+        }
+      },
+      { immediate },
+    )
+    : noop
+
+  const stop = () => {
+    cleanup()
+    stopWatch()
+    isActive.value = false
+  }
+
+  useOnCleanup(stop)
+
+  return {
+    isSupported,
+    isActive,
+    pause() {
+      cleanup()
+      isActive.value = false
+    },
+    resume() {
+      isActive.value = true
+    },
+    stop,
+  }
+}
