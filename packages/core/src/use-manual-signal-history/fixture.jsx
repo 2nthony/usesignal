f63e81fc003dc8c0015158e26d95b0eb680eed3b
2nthony <ant.hu@outlook.com>
2024-09-29 04:50:09 +0800

    feat(use-manual-signal-history): init

---
+++ packages/core/src/use-manual-signal-history/fixture.jsx
@@ -0,0 +1,80 @@
+import { useManualSignalHistory } from '.'
+import { useCounter } from '../use-counter'
+import { formatDate } from '../use-date-format'
+
+function format(ts) {
+  return formatDate(new Date(ts), 'YYYY-MM-DD HH:mm:ss')
+}
+
+export default function DemoUseManualSignalHistory() {
+  const { inc, dec, count } = useCounter()
+  const { canUndo, canRedo, history, commit, undo, redo } = useManualSignalHistory(count, { capacity: 10 })
+
+  return (
+    <div>
+      <div>
+        Count:
+        {count}
+      </div>
+
+      <button
+        onClick={() => {
+          inc()
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dec()
+        }}
+      >
+        Decrement
+      </button>
+      <span>/</span>
+      <button
+        onClick={() => {
+          commit()
+        }}
+      >
+        Commit
+      </button>
+      <button
+        disabled={!canUndo.value}
+        style={{
+          color: !canUndo.value ? 'gray' : 'inherit',
+        }}
+        onClick={() => {
+          undo()
+        }}
+      >
+        Undo
+      </button>
+      <button
+        disabled={!canRedo.value}
+        style={{
+          color: !canRedo.value ? 'gray' : 'inherit',
+        }}
+        onClick={() => {
+          redo()
+        }}
+      >
+        Redo
+      </button>
+
+      <br />
+      <br />
+
+      <p>History (limited to 10 records for demo)</p>
+
+      <div>
+        {history.value.map(i => (
+          <div key={i.timestamp}>
+            <span style={{ color: 'gray' }}>{format(i.timestamp)}</span>
+            <span style={{ marginLeft: 8, fontFamily: 'monospace' }}>{`{ value: ${i.snapshot} }`}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ packages/core/src/use-manual-signal-history/index.ts
@@ -0,0 +1,227 @@
+import type { Signal } from '../signals'
+import type { CloneFn } from '../use-cloned'
+import { useSignals } from '@preact/signals-react/runtime'
+import { useComputed, useSignal } from '../signals'
+import { cloneFnJSON } from '../use-cloned'
+import { timestamp } from '../utils'
+
+export interface UseSignalHistoryRecord<T> {
+  snapshot: T
+  timestamp: number
+}
+
+export interface UseManualSignalHistoryOptions<Raw, Serialized = Raw> {
+  /**
+   * Maximum number of history to be kept. Default to unlimited.
+   */
+  capacity?: number
+  /**
+   * Clone when taking a snapshot, shortcut for dump: JSON.parse(JSON.stringify(value)).
+   * Default to false
+   *
+   * @default false
+   */
+  clone?: boolean | CloneFn<Raw>
+  /**
+   * Serialize data into the history
+   */
+  dump?: (v: Raw) => Serialized
+  /**
+   * Deserialize data from the history
+   */
+  parse?: (v: Serialized) => Raw
+
+  /**
+   * set data source
+   */
+  setSource?: (source: Signal<Raw>, v: Raw) => void
+}
+
+export interface UseManualSignalHistoryReturn<Raw, Serialized> {
+  /**
+   * Bypassed tracking ref from the argument
+   */
+  source: Signal<Raw>
+
+  /**
+   * An array of history records for undo, newest comes to first
+   */
+  history: Signal<UseSignalHistoryRecord<Serialized>[]>
+
+  /**
+   * Last history point, source can be different if paused
+   */
+  last: Signal<UseSignalHistoryRecord<Serialized>>
+
+  /**
+   * Same as {@link UseManualSignalHistoryReturn.history | history}
+   */
+  undoStack: Signal<UseSignalHistoryRecord<Serialized>[]>
+
+  /**
+   * Records array for redo
+   */
+  redoStack: Signal<UseSignalHistoryRecord<Serialized>[]>
+
+  /**
+   * A ref representing if undo is possible (non empty undoStack)
+   */
+  canUndo: Signal<boolean>
+
+  /**
+   * A ref representing if redo is possible (non empty redoStack)
+   */
+  canRedo: Signal<boolean>
+
+  /**
+   * Undo changes
+   */
+  undo: () => void
+
+  /**
+   * Redo changes
+   */
+  redo: () => void
+
+  /**
+   * Clear all the history
+   */
+  clear: () => void
+
+  /**
+   * Create a new history record
+   */
+  commit: () => void
+
+  /**
+   * Reset ref's value with latest history
+   */
+  reset: () => void
+}
+
+function fnBypass<F, T>(v: F) {
+  return v as unknown as T
+}
+function fnSetSource<F>(source: Signal<F>, value: F) {
+  return source.value = value
+}
+
+type FnCloneOrBypass<F, T> = (v: F) => T
+
+function defaultDump<R, S>(clone?: boolean | CloneFn<R>) {
+  return (clone
+    ? typeof clone === 'function'
+      ? clone
+      : cloneFnJSON
+    : fnBypass
+  ) as unknown as FnCloneOrBypass<R, S>
+}
+
+function defaultParse<R, S>(clone?: boolean | CloneFn<R>) {
+  return (clone
+    ? typeof clone === 'function'
+      ? clone
+      : cloneFnJSON
+    : fnBypass
+  ) as unknown as FnCloneOrBypass<S, R>
+}
+
+/**
+ * Track the change history of a ref, also provides undo and redo functionality.
+ *
+ * @see https://vueuse.org/useManualRefHistory
+ * @param source
+ * @param options
+ */
+export function useManualSignalHistory<Raw, Serialized = Raw>(
+  source: Signal<Raw>,
+  options: UseManualSignalHistoryOptions<Raw, Serialized> = {},
+): UseManualSignalHistoryReturn<Raw, Serialized> {
+  useSignals()
+
+  const {
+    clone = false,
+    dump = defaultDump<Raw, Serialized>(clone),
+    parse = defaultParse<Raw, Serialized>(clone),
+    setSource = fnSetSource,
+  } = options
+
+  function _createHistoryRecord(): UseSignalHistoryRecord<Serialized> {
+    return ({
+      snapshot: dump(source.value),
+      timestamp: timestamp(),
+    })
+  }
+
+  const last: Signal<UseSignalHistoryRecord<Serialized>> = useSignal(_createHistoryRecord()) as Signal<UseSignalHistoryRecord<Serialized>>
+
+  const undoStack: Signal<UseSignalHistoryRecord<Serialized>[]> = useSignal([])
+  const redoStack: Signal<UseSignalHistoryRecord<Serialized>[]> = useSignal([])
+
+  const _setSource = (record: UseSignalHistoryRecord<Serialized>) => {
+    setSource(source, parse(record.snapshot))
+    last.value = record
+  }
+
+  const commit = () => {
+    undoStack.value = [last.value, ...undoStack.value]
+    last.value = _createHistoryRecord()
+
+    if (options.capacity && undoStack.value.length > options.capacity) {
+      undoStack.value = undoStack.value.slice(0, options.capacity)
+    }
+    if (redoStack.value.length) {
+      redoStack.value = []
+    }
+  }
+
+  const clear = () => {
+    undoStack.value = []
+    redoStack.value = []
+  }
+
+  const undo = () => {
+    const state = undoStack.value[0]
+    undoStack.value = undoStack.value.slice(1)
+
+    if (state) {
+      redoStack.value = [last.value, ...redoStack.value]
+      _setSource(state)
+    }
+  }
+
+  const redo = () => {
+    const state = redoStack.value[0]
+    redoStack.value = redoStack.value.slice(1)
+
+    if (state) {
+      undoStack.value = [last.value, ...undoStack.value]
+      _setSource(state)
+    }
+  }
+
+  const reset = () => {
+    _setSource(last.value)
+  }
+
+  const history = useComputed(() => [last.value, ...undoStack.value])
+
+  const canUndo = useComputed(() => undoStack.value.length > 0)
+  const canRedo = useComputed(() => redoStack.value.length > 0)
+
+  return {
+    source,
+    undoStack,
+    redoStack,
+    last,
+    history,
+    canUndo,
+    canRedo,
+
+    clear,
+    commit,
+    reset,
+    undo,
+    redo,
+  }
+}
