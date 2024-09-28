cf0e73458639e1a72acf3fe3acb49e2601ee451a
2nthony <ant.hu@outlook.com>
2024-09-29 00:12:09 +0800

    docs(use-watch-effect): update immediate

--- packages/core/src/use-watch/immediate.fixture.tsx
+++ packages/core/src/use-watch/immediate.fixture.tsx
@@ -2,10 +2,11 @@ import { useWatch } from '.'
 import { useSignal } from '../signals'
 
 export default function DemoUseWatchImmediate() {
-  const count1 = useSignal(0)
+  const count1 = useSignal(1)
   const count2 = useSignal(0)
 
   useWatch(count1, () => {
+    count2.value = count1.value
     // eslint-disable-next-line no-console
     console.log('count1', count1.value, 'count2', count2.value)
   }, { immediate: true })
