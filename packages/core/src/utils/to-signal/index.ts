138d2e3b866648639eeefc88ae67086842f592eb
2nthony <ant.hu@outlook.com>
2024-10-09 02:54:37 +0800

    refactor(utils): to signal

--- packages/core/src/utils/to-signal/index.ts
+++ packages/core/src/utils/to-signal/index.ts
@@ -1,6 +1,9 @@
-import type { MaybeSignalOrGetter } from '../types'
+import type { ComputedSignal, Signal } from '../../signals'
+import type { MaybeSignal } from '../types'
 import { computed, signal } from '../../signals'
 
-export function toSignal<T>(r: MaybeSignalOrGetter<T>) {
-  return (typeof r === 'function' ? computed : signal)(r as any)
+export function toSignal<T>(r: MaybeSignal<T>): Signal<T>
+export function toSignal<T>(r: () => T): ComputedSignal<T>
+export function toSignal(r: any) {
+  return (typeof r === 'function' ? computed : signal)(r)
 }
