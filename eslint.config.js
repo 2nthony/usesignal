faccfdeeb0eef8ad9781f62c877e5a3d070f5ee9
2nthony <ant.hu@outlook.com>
2024-10-09 01:53:48 +0800

    chore: eslint add react hooks rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,20 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
+import { fixupPluginRules } from '@eslint/compat'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
 
 export default antfu(
   {
     type: 'lib',
+  },
+  {
+    plugins: {
+      // @ts-ignore
+      'react-hooks': fixupPluginRules(pluginReactHooks),
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+    },
   },
   {
     rules: {
